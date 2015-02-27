40af066f36c5dee8cab51c4dd17bce3ff781276c
Jay <jhd@fastmail.net>
2015-02-27 13:25:23 -0500

    add post-question without user-auth, error handling

--- shaq_overflow/app/controllers/questions_controller.rb
+++ shaq_overflow/app/controllers/questions_controller.rb
@@ -9,6 +9,22 @@ class QuestionsController < ApplicationController
     @comment = Comment.new
     @answer = Answer.new
   end
+  
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.save
+    redirect_to '/'
+
+  end
+
+  private 
+    def question_params
+      params.require(:question).permit(:user_id, :url, :title)
+    end
 
 
 end
