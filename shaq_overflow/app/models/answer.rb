d9bafbbafc4011b3b4057f4be9fdb337146a7aab
Anna Taberski <aftaberski@gmail.com>
2015-02-27 15:56:48 -0500

    Add ability to only select one best answer

--- shaq_overflow/app/models/answer.rb
+++ shaq_overflow/app/models/answer.rb
@@ -1,6 +1,13 @@
 class Answer < ActiveRecord::Base
+  before_save :reset_chosen_answer
+
   belongs_to :user
   belongs_to :question
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
+
+  def reset_chosen_answer
+    question = Question.find(self.question_id)
+    question.reset_all_answers
+  end
 end
