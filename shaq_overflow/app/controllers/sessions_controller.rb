550cf4e82129d2d7944b2f7bb5c822725ff05d3b
Lucas Santos <lucasrsantos1@gmail.com>
2015-02-28 13:37:17 -0500

    Fix failed login problem

--- shaq_overflow/app/controllers/sessions_controller.rb
+++ shaq_overflow/app/controllers/sessions_controller.rb
@@ -6,10 +6,15 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:user][:username]).try(:authenticate, params[:user][:password])
-    set_session
     respond_to do |format|
-      format.html { redirect_to @user }
-      format.json { render json: :user, status: :logged, location: @user }
+      if @user
+        set_session
+        format.html { redirect_to @user }
+        format.json { render json: :user, status: :logged, location: @user }
+      else
+        format.html { redirect_to login_path }
+        format.json { render json: @user.errors }
+      end
     end
   end
 
