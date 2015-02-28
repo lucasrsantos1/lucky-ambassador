d4fd4c0e1435ae3d527be87fba745725c9e9df1b
David Deneroff <dgdeneroff@gmail.com>
2015-02-28 14:52:17 -0500

    Add conditional logic for up and down votes

--- shaq_overflow/app/controllers/votes_controller.rb
+++ shaq_overflow/app/controllers/votes_controller.rb
@@ -2,11 +2,18 @@ class VotesController < ApplicationController
 
   def create
     if request.xhr?
-      vote = Vote.create(votable_type: params[:votableType], user_id: current_user.id, value: 1, votable_id: params[:answerId])
-      answer = Answer.find(params[:answerId])
-      votes = answer.votes.count
-      votes.to_json
-      render json: votes
+      if params[:direction] == "up"
+        vote = Vote.create(votable_type: params[:votableType], user_id: current_user.id, value: 1, votable_id: params[:answerId])
+        @answer = Answer.find(params[:answerId])
+      else
+        vote = Vote.create(votable_type: params[:votableType], user_id: current_user.id, value: -1, votable_id: params[:answerId])
+        @answer = Answer.find(params[:answerId])
+      end
+        @vote_values = []
+        @answer.votes.each {|vote| @vote_values << vote.value.to_i}
+           @anna = @vote_values.reduce(:+)
+        @anna.to_json
+        render json: @anna
     end
   end
 
