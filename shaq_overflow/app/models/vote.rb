d5222390379f7856b5243954a28493364d897d0f
David Deneroff <dgdeneroff@gmail.com>
2015-02-28 17:18:14 -0500

    Fix commented out section

--- shaq_overflow/app/models/vote.rb
+++ shaq_overflow/app/models/vote.rb
@@ -4,7 +4,7 @@ class Vote < ActiveRecord::Base
   belongs_to :question
   belongs_to :comment
   belongs_to :answer
-  # validates :user, uniqueness: { scope: [:votable_type, :votable_id], message: "User can only rate this once" }
+  validates :user, uniqueness: { scope: [:votable_type, :votable_id], message: "User can only rate this once" }
   validates :votable_id, :votable_type, :user_id, :value, presence: true
 
 end
