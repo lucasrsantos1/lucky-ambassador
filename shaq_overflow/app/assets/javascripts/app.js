dbfc34db0f4413c10205357ed423b244827c6388
David Deneroff <dgdeneroff@gmail.com>
2015-02-28 16:10:14 -0500

    Add ajax call for questions

--- shaq_overflow/app/assets/javascripts/app.js
+++ shaq_overflow/app/assets/javascripts/app.js
@@ -3,15 +3,30 @@ $(document).ready(function() {
  $("a.up-vote-answer").click(function(event) {
       event.preventDefault();
       var $target = $(event.target);
+      $.ajax({
+        type: "post",
+        url: $target.data("url"),
+        data: $target.data(),
+        success: function(response) {
+          $('#' + $target.data('answer-id')).empty();
+          $('#' + $target.data('answer-id')).append(response)
+        }
+      });
+    }
+  );
+
+ $("a.up-vote-question").click(function(event) {
+      event.preventDefault();
+      var $target = $(event.target);
       console.log($target.data("url"))
       $.ajax({
         type: "post",
         url: $target.data("url"),
         data: $target.data(),
-        success:
-        function(response) {
-          $('#' + $target.data('answer-id')).empty();
-          $('#' + $target.data('answer-id')).append(response)
+        success: function(response) {
+          console.log(response);
+          $('#' + $target.data('question-id')).empty();
+          $('#' + $target.data('question-id')).append(response)
         }
       });
     }
