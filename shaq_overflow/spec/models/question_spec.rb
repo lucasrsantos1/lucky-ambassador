ccdc6448de8c256617da8711fefc2ad384889cbf
Jay <jhd@fastmail.net>
2015-03-01 23:21:43 -0500

    removed 'pending' templates

--- shaq_overflow/spec/models/question_spec.rb
+++ shaq_overflow/spec/models/question_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ # pending "add some examples to (or delete) #{__FILE__}"
 end
 
 describe Question do
