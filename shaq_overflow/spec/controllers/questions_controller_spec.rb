a31f3562f7f44300ebe9e75eac510ee595314a10
Jay <jhd@fastmail.net>
2015-03-01 23:22:07 -0500

    add beginning of controller test (couldn't get it working)

---
+++ shaq_overflow/spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'spec_helper'
+
+# describe QuestionsController do  
+#   let (:user) {User.create(username: 'jay', email: 'jay@j.com', password: 'abc')}
+#   let (:question) {Question.create(user_id: user.id, url: "http://www.w.com", title: "Title")}
+
+#   it "index" do
+#     pending
+#     # get :index
+#     # expect(assigns(:questions)).to eq Question.all
+#   end
+# end
+
+# describe "admin panel" do
+#     it "#index" do
+#       get :index
+#       expect(assigns(:posts)).to eq Post.all
+#     end
